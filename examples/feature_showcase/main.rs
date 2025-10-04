885e8800f20ba96f5552c86237cce6988cb1f05f
Jareth Gomes <jarethgomes@gmail.com>
2025-10-04 03:08:48 +0000

    chore: Add example command to feature_showcase

--- examples/feature_showcase/main.rs
+++ examples/feature_showcase/main.rs
@@ -54,6 +54,7 @@ async fn main() {
             checks::delete(),
             checks::ferrisparty(),
             checks::cooldowns(),
+            checks::burstable_cooldown(),
             checks::minmax(),
             checks::get_guild_name(),
             checks::only_in_dms(),
